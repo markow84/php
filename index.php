b13096eb8243cfab799ef9306a80c84d4fcc594d
Vatiaz <102673031+markow84@users.noreply.github.com>
2022-12-04 14:05:30 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+        $ile=0;
+            do{
+                $liczba_losowa=rand(1,30);
+                ++$ile;
+                echo $liczba_losowa;
+                if($liczba_losowa!==23);
+                echo ", ";
+            }
+            while($liczba_losowa!==23);
+            echo"<br>Wylosowano liczbę 23 za ".$ile." razem";
+            
+       
+        ?>
